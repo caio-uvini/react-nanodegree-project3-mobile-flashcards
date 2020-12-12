b5beef9c5b0476c1b44104b11cc5f58baf427668
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-12-12 08:57:19 -0300

    feat: add card screen

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,12 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+
+import { CommonActions } from '@react-navigation/native';
 
 class AddCard extends Component {
 
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  onTextChange = (text, fieldName) => {
+    this.setState(() => ({
+      [fieldName]: text
+    }));
+  }
+
+  submitCard = () => {
+    // TODO: actually create a new card
+    this.goToHome();
+  }
+
+  goToHome = () => {
+    this.props.navigation.dispatch(
+      CommonActions.goBack({
+        key: 'AddCard',
+      }))
+  }
+
+  isFormValid = () => {
+    const { question, answer } = this.state;
+
+    if (!question.trim()) {
+      return false;
+    }
+
+    if (!answer.trim()) {
+      return false;
+    }
+
+    return true;
+  }
+
   render() {
+
+    const { question, answer } = this.state;
+
     return (
       <View>
-        <Text>AddCard</Text>
+        <TextInput
+          onChangeText={text => this.onTextChange(text, 'question')}
+          placeholder={'Question'}
+          value={question}
+        />
+
+        <TextInput
+          onChangeText={text => this.onTextChange(text, 'answer')}
+          placeholder={'Answer'}
+          value={answer}
+        />
+
+        <TouchableOpacity
+          onPress={this.submitCard}
+          disabled={!this.isFormValid()}
+        >
+          <Text>Sumit</Text>
+        </TouchableOpacity>
       </View>
     );
   }
