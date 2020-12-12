958edbc3322c8070b72dec52e0b9f12f3b8efd75
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-12-12 09:00:49 -0300

    feat: add deck screen

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+
+import { CommonActions } from '@react-navigation/native';
 
 class AddDeck extends Component {
+
+  state = {
+    name: ''
+  }
+
+  onNameChange = (text) => {
+    this.setState(() => ({
+      name: text
+    }));
+  }
+
+  submitDeck = () => {
+    // TODO: actually create a new deck
+    this.goToHome();
+  }
+
+  goToHome = () => {
+    this.props.navigation.dispatch(
+      CommonActions.goBack({
+        key: 'AddDeck',
+      }))
+  }
+
+  isFormValid = () => {
+    return this.state.name.trim();
+  }
 
   render() {
     return (
       <View>
-        <Text>AddDeck</Text>
+        <Text>What is the title of your new deck?</Text>
+        <TextInput
+          onChangeText={this.onNameChange}
+          placeholder={'Deck Name'}
+          value={this.state.name}
+        />
+
+        <TouchableOpacity
+          onPress={this.submitDeck}
+          disabled={!this.isFormValid()}
+        >
+          <Text>Sumit</Text>
+        </TouchableOpacity>
       </View>
     );
   }
