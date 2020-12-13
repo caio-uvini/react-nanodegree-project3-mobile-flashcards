29dbe7e11ece7f6d09ea8ce4b7d211554b6a96fe
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-12-13 14:40:15 -0300

    fix: show proper content when decklist is empty

--- components/DeckList.js
+++ components/DeckList.js
@@ -8,50 +8,6 @@ import * as DeckActions from '../actions/deck';
 import DeckListItem from './DeckListItem';
 import { getDecks } from '../utils/api';
 
-const ITEMS = [
-  {
-    id: 'deck1',
-    name: 'deck1',
-    cardsCount: 1
-  }, {
-    id: 'deck2',
-    name: 'deck2',
-    cardsCount: 2
-  }, {
-    id: 'deck3',
-    name: 'deck3',
-    cardsCount: 3
-  }, {
-    id: 'deck4',
-    name: 'deck4',
-    cardsCount: 4
-  }, {
-    id: 'deck5',
-    name: 'deck5',
-    cardsCount: 5
-  }, {
-    id: 'deck6',
-    name: 'deck6',
-    cardsCount: 6
-  }, {
-    id: 'deck7',
-    name: 'deck7',
-    cardsCount: 7
-  }, {
-    id: 'deck8',
-    name: 'deck8',
-    cardsCount: 8
-  }, {
-    id: 'deck9',
-    name: 'deck9',
-    cardsCount: 9
-  }, {
-    id: 'deck10',
-    name: 'deck10',
-    cardsCount: 10
-  }
-];
-
 class DeckList extends Component {
 
   state = {
@@ -85,9 +41,16 @@ class DeckList extends Component {
       />
     }
 
+    if (decks.length == 0) {
+      return (
+        <View>
+          <Text>You don't have any deck yet! Add some and start studying!</Text>
+        </View>
+      )
+    }
+
     return (
       <View>
-        <Text>DeckList</Text>
         <FlatList
           data={decks}
           renderItem={({ item }) => this.renderItem(item, navigation)}
