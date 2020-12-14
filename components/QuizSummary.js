2e74fac918da0884a1d35528d12f2ea25cc8b1be
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-12-13 22:50:17 -0300

    feat: add styles to quiz summary view

--- components/QuizSummary.js
+++ components/QuizSummary.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 import { clearLocalNotifications, setLocalNotification } from '../utils/notifications';
+import TextButton from './TextButton';
+import { green } from '../utils/colors';
 
 class QuizSummary extends Component {
 
@@ -14,19 +17,53 @@ class QuizSummary extends Component {
     const { correctCount, totalCount, onRestart, onGoBack } = this.props;
 
     return (
-      <View>
-        <Text>Quiz completed! 🎉</Text>
-        <Text>You've answered correctly {correctCount} of {totalCount} questions!</Text>
-        <TouchableOpacity onPress={onRestart}>
-          <Text>Restart Quiz</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <AntDesign name='checkcircleo' size={80} color={green}/>
+        <Text style={styles.title}>Quiz completed!</Text>
+        
+        <View style={styles.score}>
+          <Text style={styles.scoreText}>Correct Answers: {correctCount}</Text>
+          <Text style={styles.scoreText}>Answered Questions: {totalCount}</Text>
+        </View>
+        
+        <View style={styles.buttonsContainer}>
+          <TextButton onPress={onRestart} style={styles.button}>
+            Restart Quiz
+          </TextButton>
 
-        <TouchableOpacity onPress={onGoBack}>
-          <Text>Back to Deck</Text>
-        </TouchableOpacity>
+          <TextButton onPress={onGoBack} style={[styles.button, {marginLeft: 10}]}>
+            Back to Deck
+          </TextButton>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  score: {
+    margin: 60,
+    alignItems: 'center'
+  },
+  scoreText: {
+    fontSize: 20,
+  },
+  button: {
+    width: 130,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
 
 export default QuizSummary;
