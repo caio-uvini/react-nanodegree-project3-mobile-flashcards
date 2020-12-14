2c25227da4952136507d2fcef8acab0579f1ed98
Caio Uvini <caio.uvini@quintoandar.com.br>
2020-12-13 21:03:35 -0300

    feat: add styles to add card view

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { connect } from 'react-redux';
 
@@ -7,6 +7,8 @@ import { CommonActions } from '@react-navigation/native';
 
 import { addCardToDeck } from '../utils/api';
 import * as CardActions from '../actions/card';
+import TextInput from './TextInput';
+import TextButton from './TextButton';
 
 class AddCard extends Component {
 
@@ -56,29 +58,40 @@ class AddCard extends Component {
     const { question, answer } = this.state;
 
     return (
-      <View>
+      <View style={styles.container}>
         <TextInput
           onChangeText={text => this.onTextChange(text, 'question')}
           placeholder={'Question'}
           value={question}
+          style={styles.input}
         />
 
         <TextInput
           onChangeText={text => this.onTextChange(text, 'answer')}
           placeholder={'Answer'}
           value={answer}
+          style={styles.input}
         />
 
-        <TouchableOpacity
-          onPress={this.submitCard}
-          disabled={!this.isFormValid()}
-        >
-          <Text>Sumit</Text>
-        </TouchableOpacity>
+        <TextButton onPress={this.submitCard} disabled={!this.isFormValid()}>
+          Submit
+        </TextButton>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  input: {
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  }
+});
 
 const mapStateToProps = () => ({});
 
